40d6c4088d22fd5b9b8a71571e13975c3ba8fca7
Joe <joefuller042@gmail.com>
2023-06-05 12:36:05 +0100

    update navbar

--- components/navBar.js
+++ components/navBar.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <ul className="items-center justify-center space-y-8 md:flex md:space-y-0 text-2xl text-darkGrey">
-      <li className="p-4 hover:bg-indigo-300">
+      <li className="bg-lighterGrey p-4 hover:bg-lightGrey hover:text-offWhite">
         <Link href="/">Home</Link>
       </li>
-      <li className="p-4 hover:bg-indigo-300">
+      <li className="bg-lighterGrey p-4 hover:bg-lightGrey hover:text-offWhite">
         <Link href="/about">About</Link>
       </li>
-      <li className="p-4 hover:bg-indigo-300">
+      <li className="bg-lighterGrey p-4 hover:bg-lightGrey hover:text-offWhite">
         <Link href="/projects">Projects</Link>
       </li>
     </ul>
