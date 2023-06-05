ca0c68546f6e75a209c0023171d1822b1617ad19
Joe <joefuller042@gmail.com>
2023-06-05 12:27:51 +0100

    update global background colour

--- app/about/page.js
+++ app/about/page.js
@@ -2,7 +2,7 @@ import NavBar from "@/components/navBar";
 
 export default function About() {
   return (
-    <div>
+    <div className="bg-indigo-500">
       <NavBar></NavBar>
       <p>I'm a new guy</p>
     </div>
