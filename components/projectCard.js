b426ba113bff433a6a98e3abce927a231d38b185
Joe <joefuller042@gmail.com>
2023-06-05 15:23:38 +0100

    add border to project card

--- components/projectCard.js
+++ components/projectCard.js
@@ -1,6 +1,9 @@
 export default function ProjectCard({ title, url, description }) {
   return (
-    <a href={url} className="bg-lighterGrey rounded p-4">
+    <a
+      href={url}
+      className="bg-lighterGrey rounded p-4 border-lightGrey border-2"
+    >
       <h3 className="text-xl">{title}</h3>
       <p>{description}</p>
     </a>
