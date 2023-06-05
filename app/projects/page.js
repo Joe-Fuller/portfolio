ab8616b610810a286a924a9dd319bc231046528c
Joe <joefuller042@gmail.com>
2023-06-05 15:17:27 +0100

    create wikipedia game project card

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,14 +1,18 @@
 import NavBar from "@/components/navBar";
+import ProjectCard from "@/components/projectCard";
+import Socials from "@/components/socials";
 
 export default function Projects() {
   return (
     <div className="grid justify-center text-darkGrey">
       <NavBar></NavBar>
+      <Socials></Socials>
       <h1 className="text-2xl">Projects</h1>
-      <ul className="grid grid-cols-2 place-self-center">
-        <li>Wikipedia Game</li>
-        <li>All Aboard</li>
-      </ul>
+      <ProjectCard
+        title="Wikipedia Game"
+        url="https://the-wikipedia-game.netlify.app/"
+        description="A browser game built using React. Try to get from one Wikipedia article to another using only links!"
+      ></ProjectCard>
     </div>
   );
 }
