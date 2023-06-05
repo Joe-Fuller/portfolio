cdd52f126b9e9305dff2931d5150c0efac431d18
Joe <joefuller042@gmail.com>
2023-06-05 11:42:12 +0100

    update navbar css

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
           <p className="text-center text-xl">
             I am a 26 year old full-stack developer looking for my first tech
             role. I have experience in JavaScript, SQL, C#, and Python as well
-            as React, NextJS, Unity, Godot, and more!
+            as React, NextJS, Tailwind, Unity, Godot, and more!
           </p>
         </div>
       </div>
--- components/navBar.js
+++ components/navBar.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function NavBar() {
-  return <div>Nav Bar</div>;
+  return (
+    <ul className="items-center justify-center space-y-8 md:flex md:space-y-0 text-2xl">
+      <li className="bg-indigo-500 p-4 hover:bg-indigo-300">
+        <Link href="/">Home</Link>
+      </li>
+      <li className="bg-indigo-500 p-4 hover:bg-indigo-300">
+        <Link href="/about">About</Link>
+      </li>
+      <li className="bg-indigo-500 p-4 hover:bg-indigo-300">
+        <Link href="/projects">Projects</Link>
+      </li>
+    </ul>
+  );
 }
