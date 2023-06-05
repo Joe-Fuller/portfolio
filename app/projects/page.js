38d9417fb4fb5f34dee602ee11d011669c4cb3c5
Joe <joefuller042@gmail.com>
2023-06-05 15:12:00 +0100

    update project page css

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,7 +1,14 @@
+import NavBar from "@/components/navBar";
+
 export default function Projects() {
   return (
-    <div>
-      <ul className="grid grid-cols-2 w-5/12">Projects</ul>
+    <div className="grid justify-center text-darkGrey">
+      <NavBar></NavBar>
+      <h1 className="text-2xl">Projects</h1>
+      <ul className="grid grid-cols-2 place-self-center">
+        <li>Wikipedia Game</li>
+        <li>All Aboard</li>
+      </ul>
     </div>
   );
 }
