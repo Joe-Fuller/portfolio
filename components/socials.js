72edbc7e48cbfcaf917ed53fc88f94fe26531f6a
Joe <joefuller042@gmail.com>
2023-06-05 11:02:13 +0100

    center social links

--- components/socials.js
+++ components/socials.js
@@ -2,15 +2,18 @@ import Image from "next/image";
 
 export default function Socials() {
   return (
-    <div>
-      <a href="https://github.com/Joe-Fuller">
-        <Image src="/github.png" width="40" height="40" alt="Link to GitHub" />
+    <div className="fixed inset-y-0 grid">
+      <a className="place-self-end" href="https://github.com/Joe-Fuller">
+        <Image src="/github.png" width="50" height="50" alt="Link to GitHub" />
       </a>
-      <a href="https://www.linkedin.com/in/fuller-joe/">
+      <a
+        className="place-self-start"
+        href="https://www.linkedin.com/in/fuller-joe/"
+      >
         <Image
           src="/linkedin.png"
-          width="40"
-          height="40"
+          width="50"
+          height="50"
           alt="Link to LinkedIn"
         />
       </a>
