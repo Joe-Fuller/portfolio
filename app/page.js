45b3e7b11e874b0a575bf3abe5072a60b40d7d00
Joe <joefuller042@gmail.com>
2023-06-05 10:18:06 +0100

    update css

--- app/page.js
+++ app/page.js
@@ -3,12 +3,16 @@ import Socials from "@/components/socials";
 export default function Home() {
   return (
     <main>
-      <h1>Joe Fuller Portfolio</h1>
-      <p>
-        I am a 26 year old full-stack developer looking for my first role. I
-        have experience in JavaScript, SQL, C#, and Python as well as React,
-        NextJS, Unity, Godot, and more!
-      </p>
+      <div className="grid h-screen place-content-center">
+        <h1 className="text-center text-3xl">Joe Fuller</h1>
+        <div className="place-self-center w-7/12">
+          <p className="text-center text-xl">
+            I am a 26 year old full-stack developer looking for my first tech
+            role. I have experience in JavaScript, SQL, C#, and Python as well
+            as React, NextJS, Unity, Godot, and more!
+          </p>
+        </div>
+      </div>
       <Socials></Socials>
     </main>
   );
