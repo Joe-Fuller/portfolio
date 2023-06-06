01b668b749429f422902b1f815b1de1c24679963
Joe <joefuller042@gmail.com>
2023-06-06 13:42:07 +0100

    lighten project card on hover

--- components/projectCard.js
+++ components/projectCard.js
@@ -2,7 +2,7 @@ export default function ProjectCard({ title, url, description }) {
   return (
     <a
       href={url}
-      className="bg-lighterGrey rounded p-4 border-lightGrey border-2"
+      className="bg-lighterGrey rounded p-4 border-lightGrey border-2 hover:bg-offWhite"
     >
       <h3 className="text-xl">{title}</h3>
       <p>{description}</p>
