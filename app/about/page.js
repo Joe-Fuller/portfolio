45532b47be59cf3c431cd54b12e02586f55c41ff
Joe <joefuller042@gmail.com>
2023-06-06 13:54:59 +0100

    add my face to about section

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,6 @@
 import NavBar from "@/components/navBar";
 import Socials from "@/components/socials";
+import Image from "next/image";
 
 export default function About() {
   return (
@@ -7,6 +8,14 @@ export default function About() {
       <NavBar></NavBar>
       <Socials></Socials>
       <div className="grid h-screen place-content-center text-darkGrey text-center">
+        <Image
+          className="place-self-center"
+          src="/JoesFace.jpg"
+          width="200"
+          height="200"
+          alt="Joe Fuller's face"
+        />
+        <p>A picture of me in Japan, April 2023</p>
         <p className="w-7/12 place-self-center text-xl">
           I am a 26 year old full-stack developer looking for my first tech
           role. I have experience in JavaScript, SQL, C#, and Python as well as
